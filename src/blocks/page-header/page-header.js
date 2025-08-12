9af612d993d80a4876c37a37a38b37c8087c1408
Igor <des.yogi@gmail.com>
2025-08-12 15:29:21 +0300

    index 4 stage

--- src/blocks/page-header/page-header.js
+++ src/blocks/page-header/page-header.js
@@ -1,4 +1,4 @@
-(function (){
+/*(function (){
   let new_scroll_position = 0;
   let last_scroll_position;
   const header = document.getElementById('header');
@@ -22,4 +22,43 @@
   };
 
   window.addEventListener('scroll', scrollHandler, { passive: true });
-})()
+})()*/
+(function () {
+  const HIDE_HEADER_SCROLL = 140;
+  let new_scroll_position = 0;
+  let last_scroll_position;
+  const header = document.getElementById('header');
+  if (!header) return;
+
+  function clearHeaderClasses() {
+    header.classList.remove('page-header--slideUp', 'page-header--slideDown');
+  }
+
+  function scrollHandler() {
+    last_scroll_position = window.scrollY;
+
+    if (last_scroll_position === 0) {
+      clearHeaderClasses();
+    } else if (new_scroll_position < last_scroll_position && last_scroll_position > HIDE_HEADER_SCROLL) {
+      header.classList.remove('page-header--slideDown');
+      header.classList.add('page-header--slideUp');
+    } else if (new_scroll_position > last_scroll_position) {
+      header.classList.remove('page-header--slideUp');
+      header.classList.add('page-header--slideDown');
+    }
+
+    new_scroll_position = last_scroll_position;
+  }
+
+  // requestAnimationFrame-throttle
+  let ticking = false;
+  window.addEventListener('scroll', function() {
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        scrollHandler();
+        ticking = false;
+      });
+      ticking = true;
+    }
+  }, { passive: true });
+})();
